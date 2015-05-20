a29243c47fb1d6e76fbb2eaf5d3aa09ed8adec3c
Matthew Francis Brunetti <zenflow87@gmail.com>
2015-05-19 21:48:21 -0400

    added jsdoc

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,11 @@
-/**
- * Created by Matthew on 2015-05-18.
- */
+var gulp = require('gulp');
+var del = require('del');
+var jsdoc = require('gulp-jsdoc');
+gulp.task('docs:clean', function(cb){
+	del(['docs'], cb);
+});
+gulp.task('docs', ['docs:clean'], function(){
+	return gulp.src(['lib/**.js', 'README.md'])
+		.pipe(jsdoc('docs'));
+});
+gulp.task('default', ['docs']);
