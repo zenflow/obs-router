5c179d3145a0fd7e13ccc4246572240f8fa30b8c
Matthew Francis Brunetti <zenflow87@gmail.com>
2015-05-21 20:16:58 -0400

    Added update task to docs gulpfile

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -4,9 +4,13 @@ var jsdoc = require('gulp-jsdoc');
 var package_json = require('../package.json');
 var fs = require('fs');
 var path = require('path');
+var wrench = require('wrench');
+var child_process = require('child_process');
+var find = require('lodash.find');
 var styles = '<style type="text/css">'+fs.readFileSync(path.join(__dirname, 'src/styles.css'), 'utf8')+'</style>';
 var scripts = '<script type="text/javascript">'+fs.readFileSync(path.join(__dirname, 'src/scripts.js'), 'utf8')+'</script>';
 
+var NOTHING_TO_COMMIT_MSG = 'nothing to commit, working directory clean';
 gulp.task('clean', function(cb){
 	del(['build'], cb);
 });
@@ -30,4 +34,41 @@ gulp.task('build', ['clean'], function(){
 gulp.task('watch', ['build'], function(){
 	gulp.watch(['src/styles.css', 'src/scripts.js', '../README.md', '../lib/**/**.js'], ['build'])
 });
+gulp.task('update', ['build'], function(cb){
+	exec('git status', function(error, status_str){
+		if (error){return cb(error);}
+		var status_lines = status_str.split('\n');
+		if (!find(status_lines, function(line){return line==NOTHING_TO_COMMIT_MSG; })){
+			return cb(new Error('Working copy must be clean'));
+		}
+		var match = status_lines[0].match(/^On branch (.*)$/);
+		if (!match){
+			return cb(new Error('Could not determine current branch'));
+		}
+		var previous_branch = match[1];
+		exec('git checkout gh-pages', function(error){
+			if (error){return cb(error);}
+			wrench.copyDirRecursive('build/'+package_json.name+'/'+package_json.version+'/', '../', function(error){
+				if (error){return cb(error);}
+				exec('git add ../ && git commit -m "automatic update"', function(error){
+					if (error){return cb(error);}
+					exec('git checkout '+previous_branch, cb);
+				});
+			})
+		});
+	});
+});
 gulp.task('default', ['build']);
+
+function exec(cmd, cb){
+	console.log('$ '+cmd);
+	child_process.exec(cmd, function(error, stdout, stderr){
+		console.log(stdout);
+		if (error){
+			console.error(stderr);
+			cb(error);
+		} else {
+			cb(null, stdout);
+		}
+	});
+}
